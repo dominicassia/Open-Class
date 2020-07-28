dd2ee77cce8b55b94e0c61223f39b9c3d807897b
dominic <dominicassia@gmail.com>
2020-07-28 09:29:27 -0400

    Setup

---
+++ bb-collab.py
@@ -0,0 +1,12 @@
+import selenium
+import time
+
+def main():
+
+    ''' The goal of this program is to open class at a specific time '''
+
+    url = 'https://us.bbcollab.com/collab/ui/session/guest/c46033437acc4436a8a9b106accdd2a1'
+
+
+# Call main
+main()
